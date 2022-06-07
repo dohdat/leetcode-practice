fcbb1bc8afdd2fc0c376f9669718c83f88e1216d
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-06-07 09:21:40 -0700

    Time: 190 ms (23.16%), Space: 46 MB (55.46%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.js
+++ 76-minimum-window-substring/76-minimum-window-substring.js
@@ -4,16 +4,16 @@
  * @return {string}
  */
 var minWindow = function(s, t) {
-    let res = '';
-    let resLen = Infinity;
     let countT = new Map();
     let window = new Map();
+    let res = '';
+    let resLen = Infinity;
     let have = 0;
-    let left = 0;
     for (let i = 0; i < t.length; i++) {
         countT.set(t[i], 1 + (countT.get(t[i]) || 0));
     }
     let need = countT.size;
+    let left = 0;
     for (let right = 0; right < s.length; right++) {
         let c = s[right];
         window.set(c, 1 + (window.get(c) || 0));
@@ -23,7 +23,7 @@ var minWindow = function(s, t) {
         while (have === need) {
             if (right - left + 1 < resLen) {
                 res = s.substring(left, right + 1);
-                resLen = right - left + 1;
+                resLen = res.length;
             }
             window.set(s[left], window.get(s[left]) - 1);
             if (countT.has(s[left]) && window.get(s[left]) < countT.get(s[left])) {

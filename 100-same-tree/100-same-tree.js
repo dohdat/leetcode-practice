441ebdb2e61dfde59228ccaac273a8afe58491aa
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-06-08 09:34:18 -0700

    Time: 60 ms (91.17%), Space: 42.4 MB (53.65%) - LeetHub

--- 100-same-tree/100-same-tree.js
+++ 100-same-tree/100-same-tree.js
@@ -12,11 +12,8 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    if (!q && !p) {
-        return true;
-    }
-    if (!q || !p || q.val !== p.val) {
-        return false;
-    }
-    return isSameTree(q.left, p.left) && isSameTree(q.right, p.right);
+    if (!q && !p) return true;
+    if (!q || !p) return false;
+    if (q.val !== p.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };

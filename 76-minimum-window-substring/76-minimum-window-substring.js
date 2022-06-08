8a9d18ac9188a651f7c98b29e91d1ce13131cdc4
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-06-08 09:19:01 -0700

    Time: 97 ms (85.33%), Space: 44.7 MB (83.32%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.js
+++ 76-minimum-window-substring/76-minimum-window-substring.js
@@ -6,27 +6,28 @@
 var minWindow = function(s, t) {
     let countT = new Map();
     let window = new Map();
-    let res = '';
-    let resLen = Infinity;
-    let have = 0;
     for (let i = 0; i < t.length; i++) {
         countT.set(t[i], 1 + (countT.get(t[i]) || 0));
     }
-    let need = countT.size;
     let left = 0;
+    let res = '';
+    let resLen = Infinity;
+    let have = 0;
+    let need = countT.size;
     for (let right = 0; right < s.length; right++) {
         let c = s[right];
         window.set(c, 1 + (window.get(c) || 0));
-        if (countT.has(c) && window.get(c) === countT.get(c)) {
+        if (countT.get(c) && window.get(c) === countT.get(c)) {
             have++;
         }
         while (have === need) {
+            let c = s[left];
             if (right - left + 1 < resLen) {
                 res = s.substring(left, right + 1);
                 resLen = res.length;
             }
-            window.set(s[left], window.get(s[left]) - 1);
-            if (countT.has(s[left]) && window.get(s[left]) < countT.get(s[left])) {
+            window.set(c, window.get(c) - 1);
+            if (countT.get(c) && window.get(c) < countT.get(c)) {
                 have--;
             }
             left++;

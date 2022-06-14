aa350bedfa2846ee8746e4eeaf378038b381c0e7
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-06-14 10:04:26 -0700

    Time: 1168 ms (39.44%), Space: 97.7 MB (32.90%) - LeetHub

---
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.js
@@ -0,0 +1,40 @@
+var MedianFinder = function() {
+    this.arr = [];
+};
+
+/**
+ * @param {number} num
+ * @return {void}
+ */
+MedianFinder.prototype.addNum = function(num) {
+    if (!this.arr.length) {
+        this.arr.push(num);
+        return;
+    }
+    let left = 0;
+    let right = this.arr.length;
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        num > this.arr[mid] ? (left = mid + 1) : (right = mid);
+    }
+    this.arr.splice(left, 0, num);
+};
+
+/**
+ * @return {number}
+ */
+MedianFinder.prototype.findMedian = function() {
+    let mid = Math.floor(this.arr.length / 2);
+    if (this.arr.length % 2 === 0) {
+        return (this.arr[mid] + this.arr[mid - 1]) / 2;
+    } else {
+        return this.arr[mid];
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * var obj = new MedianFinder()
+ * obj.addNum(num)
+ * var param_2 = obj.findMedian()
+ */

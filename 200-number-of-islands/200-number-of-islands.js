cab2db824917f1e05bd0227aa0a26166c3c337dc
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-06-17 10:06:34 -0700

    Time: 81 ms (95.13%), Space: 44.3 MB (96.99%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -8,7 +8,7 @@ var numIslands = function(grid) {
     let res = 0;
     function dfs(r, c) {
         if (r < 0 || c < 0 || r >= row || c >= col || grid[r][c] === '0') {
-            return;
+            return false;
         }
         grid[r][c] = '0';
         dfs(r + 1, c);

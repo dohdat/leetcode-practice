7cd18b4fd7a2cde74b8f2b92b97241874d07c949
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-06-21 13:36:26 -0700

    Time: 125 ms (46.06%), Space: 47.5 MB (59.49%) - LeetHub

--- 207-course-schedule/207-course-schedule.js
+++ 207-course-schedule/207-course-schedule.js
@@ -1,26 +1,25 @@
 var canFinish = function(numCourses, prerequisites) {
-    let preMap = new Map();
+    let adjList = new Map();
     let visited = new Set();
-    //     create an adj List
     for (let i = 0; i < numCourses; i++) {
-        preMap.set(i, []);
+        adjList.set(i, []);
     }
     prerequisites.forEach(([crs, pre]) => {
-        if (preMap.has(crs)) {
-            preMap.set(crs, [...preMap.get(crs), pre]);
+        if (adjList.has(crs)) {
+            adjList.get(crs).push(pre);
         } else {
-            preMap.set(crs, [pre]);
+            adjList.set(crs, [pre]);
         }
     });
     function dfs(crs) {
         if (visited.has(crs)) return false;
         visited.add(crs);
-        for (let i = 0; i < preMap.get(crs).length; i++) {
-            let c = preMap.get(crs)[i];
+        for (let i = 0; i < adjList.get(crs).length; i++) {
+            let c = adjList.get(crs)[i];
             if (!dfs(c)) return false;
         }
         visited.delete(crs);
-        preMap.set(crs, []);
+        adjList.set(crs, []);
         return true;
     }
     for (let i = 0; i < numCourses; i++) {

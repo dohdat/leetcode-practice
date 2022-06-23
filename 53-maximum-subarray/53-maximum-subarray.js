d72a4138ddc97482181ecf1f83ae0531a785e46f
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-06-23 09:11:24 -0700

    Time: 113 ms (54.66%), Space: 50.4 MB (54.53%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -4,13 +4,11 @@
  */
 function maxSubArray(nums) {
     let max = nums[0];
-    let curSum = 0;
+    let cur = 0;
     for (let i = 0; i < nums.length; i++) {
-        if (curSum < 0) {
-            curSum = 0;
-        }
-        curSum += nums[i];
-        max = Math.max(curSum, max);
+        if (cur < 0) cur = 0;
+        cur += nums[i];
+        max = Math.max(max, cur);
     }
     return max;
 }

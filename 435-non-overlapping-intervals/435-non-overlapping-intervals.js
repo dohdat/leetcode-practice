b6f45f89eb6c32798e0643f3cc3be60205d9e011
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-06-27 15:14:07 -0700

    Time: 237 ms (99.40%), Space: 75.8 MB (31.14%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.js
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.js
@@ -4,7 +4,6 @@
  */
 var eraseOverlapIntervals = function(intervals) {
     intervals.sort((a, b) => a[1] - b[1]);
-
     let prev = intervals[0];
     let res = 0;
     for (let i = 1; i < intervals.length; i++) {

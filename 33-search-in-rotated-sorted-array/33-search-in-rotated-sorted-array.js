13c65a5de4872921dc47bd6b55550039c410516d
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-06-30 15:13:30 -0700

    Time: 86 ms (51.83%), Space: 42.4 MB (30.88%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (target === nums[mid]) return mid;
+        //         left sorted potion?
+        if (nums[mid] >= nums[left]) {
+            //search right if target > mid val
+            if (target > nums[mid] || target < nums[left]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        //right sorted potion
+        else {
+            if (target < nums[mid] || target > nums[right]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+    }
+    return -1;
+};

9754711828198e590154e675ba33db0e1f995c81
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-01 10:03:57 -0700

    Time: 97 ms (53.55%), Space: 45.5 MB (37.13%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -12,16 +12,16 @@
  */
 var maxDepth = function(root) {
     if (!root) return 0;
-    let level = 0;
     let q = [root];
+    let levels = 0;
     while (q.length !== 0) {
-        const len = q.length;
+        let len = q.length;
         for (let i = 0; i < len; i++) {
-            let node = q.shift();
-            if (node.left) q.push(node.left);
-            if (node.right) q.push(node.right);
+            let c = q.shift();
+            c.left && q.push(c.left);
+            c.right && q.push(c.right);
         }
-        level++;
+        levels++;
     }
-    return level;
+    return levels;
 };

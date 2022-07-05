5112501f93d83e766d5b438fab70f3d7e114fd1a
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-05 11:05:22 -0700

    Time: 128 ms (46.15%), Space: 49.9 MB (7.69%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.js
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.js
@@ -14,12 +14,13 @@
 var isSubtree = function(root, subRoot) {
     if (!subRoot) return true;
     if (!root) return false;
-    if (isSameTree(root, subRoot)) return true;
+    if(isSameTree(root, subRoot)) {
+        return true;
+    }
     return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
 };
-
-var isSameTree = function(s, t) {
+function isSameTree(s, t) {
     if (!s && !t) return true;
     if (!s || !t || s.val !== t.val) return false;
     return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
-};
+}

f363862e664e5afcbbde49882c8730de72faa5cd
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-07 15:20:22 -0700

    Time: 538 ms (73.22%), Space: 42.2 MB (91.99%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -10,15 +10,18 @@ var exist = function(board, word) {
     function backtrack(i, r, c) {
         if (i === word.length) {
             res = true;
-            return;
+            return true;
         }
-        if (r < 0 || c < 0 || r >= rows || c >= cols || board[r][c] !== word[i] || !board[r][c]) return;
-
+        if (r < 0 || c < 0 || r >= rows || c >= cols || !board[r][c] || board[r][c] !== word[i]) {
+            return false;
+        }
         board[r][c] = null;
-        backtrack(i + 1, r - 1, c) || backtrack(i + 1, r + 1, c) || backtrack(i + 1, r, c - 1) || backtrack(i + 1, r, c + 1);
+        backtrack(i + 1, r - 1, c);
+        backtrack(i + 1, r + 1, c);
+        backtrack(i + 1, r, c - 1);
+        backtrack(i + 1, r, c + 1);
         board[r][c] = word[i];
     }
-
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < cols; c++) {
             if (board[r][c] === word[0]) {

31e9211f57cf4d2cecc0280afb1a8decf3591bf8
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-07 11:33:37 -0700

    Time: 131 ms (96.45%), Space: 44.3 MB (96.72%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.js
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.js
@@ -3,8 +3,10 @@
  * @return {number}
  */
 var minCostConnectPoints = function(points) {
+    //set result cost to 0
     let cost = 0;
     let n = points.length;
+    //set all distances to Infinity
     let dist = new Array(n).fill(Infinity);
     dist[0] = 0;
     let next = 0;
@@ -15,15 +17,20 @@ var minCostConnectPoints = function(points) {
             let [x1, y1] = points[j];
             let [x2, y2] = points[next];
             if (dist[j] > 0) {
+                // step 2: calculate all distances to the selected point
                 dist[j] = Math.min(dist[j], Math.abs(x1 - x2) + Math.abs(y1 - y2));
                 if (dist[j] < min) {
+                    //choose the point with the minimal distance
                     min = dist[j];
                     point = j;
                 }
             }
         }
+        //add the distance to the cost
         cost += min;
+        //exclude the point from next searches and make it selected
         dist[point] = 0;
+        //if all points connected, return the result, else go to step 2.
         next = point;
     }
     return cost;

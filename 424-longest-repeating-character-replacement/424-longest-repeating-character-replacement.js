fbb327d8fd270c2db021a716813d9fba27eb5dde
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-07 15:25:14 -0700

    Time: 262 ms (27.76%), Space: 48.9 MB (10.75%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
@@ -4,16 +4,16 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
-    let max = 0;
     let left = 0;
-    let charsMap = new Map();
-    let maxFreq = 0;
+    let map = new Map();
+    let max = 0;
     for (let right = 0; right < s.length; right++) {
         let c = s[right];
-        charsMap.set(c, charsMap.get(c) ? charsMap.get(c) + 1 : 1);
-        maxFreq = Math.max(...charsMap.values());
+        map.set(c, 1 + (map.get(c) || 0));
+        let maxFreq = Math.max(...map.values());
         while (right - left + 1 - maxFreq > k) {
-            charsMap.set(s[left], charsMap.get(s[left]) - 1);
+            let lc = s[left];
+            map.set(lc, map.get(lc) - 1);
             left++;
         }
         max = Math.max(right - left + 1, max);

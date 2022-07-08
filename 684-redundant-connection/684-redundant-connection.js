1f4a87a2081159ae560913d8ca69e3f21592f291
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-08 15:08:23 -0700

    Time: 95 ms (67.17%), Space: 44.9 MB (68.51%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.js
+++ 684-redundant-connection/684-redundant-connection.js
@@ -1,43 +1,19 @@
-/**
- * @param {number[][]} edges
- * @return {number[]}
- */
 var findRedundantConnection = function(edges) {
-    let parent = [];
-    let rank = new Array(edges.length + 1).fill(1);
+    let par = [];
     for (let i = 0; i < edges.length + 1; i++) {
-        parent.push(i);
+        par.push(i);
     }
-
-    function find(n) {
-        let p = parent[n];
-        while (p !== parent[p]) {
-            parent[p] = parent[parent[p]];
-            p = parent[p];
-        }
-        return p;
-    }
-    //return false if cant complete
     function union(n1, n2) {
-        let p1 = find(n1);
-        let p2 = find(n2);
-        if (p1 === p2) {
-            return false;
-        }
-        if (rank[p1] > rank[p2]) {
-            parent[p2] = p1;
-            rank[p1] += rank[p2];
+        return (par[find(n2)] = find(n1));
+    }
+    function find(n) {
+        if (n === par[n]) {
+            return par[n];
         } else {
-            parent[p1] = p2;
-            rank[p2] += rank[p1];
-        }
-        return true;
-    }
-
-    for (let [n1, n2] of edges) {
-        if (!union(n1, n2)) {
-            return [n1, n2];
+            return (par[n] = find(par[n]));
         }
     }
-    return [];
+    for (let [a, b] of edges)
+        if (find(a) === find(b)) return [a, b];
+        else union(a, b);
 };

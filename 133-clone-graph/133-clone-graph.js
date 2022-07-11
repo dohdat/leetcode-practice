227f2e579bda15fe8f5dfa5c4970de9ef6d6e228
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-11 15:36:35 -0700

    Time: 85 ms (62.34%), Space: 43.8 MB (68.64%) - LeetHub

--- 133-clone-graph/133-clone-graph.js
+++ 133-clone-graph/133-clone-graph.js
@@ -1,15 +1,3 @@
-/**
- * // Definition for a Node.
- * function Node(val, neighbors) {
- *    this.val = val === undefined ? 0 : val;
- *    this.neighbors = neighbors === undefined ? [] : neighbors;
- * };
- */
-
-/**
- * @param {Node} node
- * @return {Node}
- */
 var cloneGraph = function(node) {
     if (!node) return null;
     let map = new Map();

d52100b07b4fefd4fc09adb9b93dde6bf767fe2a
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-11 10:46:10 -0700

    Time: 170 ms (21.30%), Space: 49.3 MB (39.35%) - LeetHub

---
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[][]} tickets
+ * @return {string[]}
+ */
+var findItinerary = function(tickets) {
+    tickets.sort();
+    let res = [];
+    let preMap = new Map();
+    for (let [src, dst] of tickets) {
+        preMap.set(src, (preMap.get(src) || []).concat(dst).sort());
+    }
+
+    function dfs(node) {
+        let visiting = preMap.get(node);
+        while (visiting && visiting.length > 0) {
+            let c = visiting.shift();
+            dfs(c);
+        }
+        res.unshift(node);
+    }
+    dfs('JFK');
+    return res;
+};

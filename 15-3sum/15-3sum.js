30ca5003233152e5493d82961c138a8d0a0ab3f1
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-12 08:15:01 -0700

    Time: 1483 ms (13.83%), Space: 61.5 MB (17.31%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -2,30 +2,31 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+//[ -4, -1, -1, 0, 1, 2 ]
 
 var threeSum = function(nums) {
-  let res = [];
-  nums.sort((a, b) => a - b);
-  for (let i = 0; i < nums.length; i++) {
-    if (i > 0 && nums[i] === nums[i - 1]) {
-      continue;
-    }
-    let l = i + 1;
-    let r = nums.length - 1;
-    while (l < r) {
-      let target = nums[i] + nums[l] + nums[r];
-      if (target > 0) {
-        r--;
-      } else if (target < 0) {
-        l++;
-      } else {
-        res.push([nums[i], nums[l], nums[r]]);
-        l++;
-        while (nums[l] == nums[l - 1] && l < r) {
-          l++;
+    nums.sort((a, b) => a - b);
+    let res = [];
+    let set = new Set();
+    for (let i = 0; i < nums.length; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+        while (left < right) {
+            let curSum = nums[i] + nums[left] + nums[right];
+            if (curSum === 0) {
+                if (!set.has([nums[i], nums[left], nums[right]].toString())) {
+                    set.add([nums[i], nums[left], nums[right]].toString());
+                    res.push([nums[i], nums[left], nums[right]]);
+                } else {
+                    left++;
+                    right--;
+                }
+            } else if (curSum < 0) {
+                left++;
+            } else {
+                right--;
+            }
         }
-      }
     }
-  }
-  return res;
+    return res;
 };

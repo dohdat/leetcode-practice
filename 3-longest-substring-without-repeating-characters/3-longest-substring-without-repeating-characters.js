60aa1127bfed25bcd0e728ce7e868e313b2eecee
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-13 09:13:29 -0700

    Time: 80 ms (98.24%), Space: 46.9 MB (71.85%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -3,16 +3,15 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let max = 0;
     let set = new Set();
+    let max = 0;
     let left = 0;
     for (let right = 0; right < s.length; right++) {
         while (set.has(s[right])) {
-            set.delete(s[left]);
-            left++;
+            set.delete(s[left++]);
         }
         set.add(s[right]);
-        max = Math.max(right - left + 1, max);
+        max = Math.max(max, right - left + 1);
     }
     return max;
 };

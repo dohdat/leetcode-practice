070cf9727e8b9ba448455fefd0393348cdb5f9ac
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-13 14:09:28 -0700

    Time: 113 ms (61.68%), Space: 51.1 MB (5.46%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} words
+ * @param {number} k
+ * @return {string[]}
+ */
+var topKFrequent = function(words, k) {
+    let preMap = new Map();
+    let res = [];
+    for (let i = 0; i < words.length; i++) {
+        preMap.set(words[i], 1 + (preMap.get(words[i]) || 0));
+    }
+    let sorted = [...preMap].sort((a, b) => {
+        if (b[1] === a[1]) {
+            return a[0].localeCompare(b[0]);
+        }
+        return b[1] - a[1];
+    });
+    console.log(sorted)
+    for (let i = 0; i < k; i++) {
+        res.push(sorted[i][0]);
+    }
+    return res;
+};

f46f12d7f34cc9ae6e0d94bc3d4714d5cddcfaea
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-15 13:25:25 -0700

    Time: 119 ms (61.45%), Space: 45.5 MB (61.21%) - LeetHub

--- 39-combination-sum/39-combination-sum.js
+++ 39-combination-sum/39-combination-sum.js
@@ -5,22 +5,19 @@
  */
 var combinationSum = function(candidates, target) {
     let res = [];
-    function backtrack(i, path, total) {
-        if (total === target) {
-            res.push([...path]);
+    function backtrack(i, path, curSum) {
+        if (curSum === target) {
+            res.push(path.slice());
             return;
         }
-        if (total > target || i >= candidates.length) {
+        if (i >= candidates.length || curSum > target) {
             return;
         }
-        //1st case: track if we can use this el 
         path.push(candidates[i]);
-        backtrack(i, path, total + candidates[i]);
+        backtrack(i, path, curSum + candidates[i]);
         path.pop();
-        //2nd case: track if we cannot use this el
-        backtrack(i + 1, path, total);
+        backtrack(i + 1, path, curSum);
     }
-
     backtrack(0, [], 0);
     return res;
 };

e2eec9d4848a33f2dfa7bc757662c741df6f402d
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-15 10:14:20 -0700

    Time: 135 ms (15.71%), Space: 45.1 MB (43.39%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.js
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -1,25 +1,14 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-
-/**
- * @param {ListNode} head
- * @return {boolean}
- */
+//Fast and slow pointers algo
 var hasCycle = function(head) {
-    if (head === null) return false;
     let slow = head;
-    let fast = head.next;
-    while (slow !== fast) {
-        if (fast === null || fast.next === null) {
-            return false;
-        }
+    let fast = head;
+    while (fast && fast.next) {
         slow = slow.next;
         fast = fast.next.next;
+        if (slow === fast) return true;
     }
-    return true;
+    return false;
 };
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)

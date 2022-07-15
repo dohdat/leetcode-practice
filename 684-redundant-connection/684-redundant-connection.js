b5cd93e4c737737cf6e78fac45beb2c2b2e744ad
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-15 13:12:41 -0700

    Time: 108 ms (50.17%), Space: 44.9 MB (63.18%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.js
+++ 684-redundant-connection/684-redundant-connection.js
@@ -1,10 +1,16 @@
+/**
+ * @param {number[][]} edges
+ * @return {number[]}
+ */
 var findRedundantConnection = function(edges) {
+    //undirected graph, use union find to detect cycle
     let parent = [];
+
     let rank = new Array(edges.length + 1).fill(1);
     for (let i = 0; i < edges.length + 1; i++) {
         parent.push(i);
     }
-
+    //function to find the parent of n
     function find(n) {
         let p = parent[n];
         while (p !== parent[p]) {
@@ -12,13 +18,16 @@ var findRedundantConnection = function(edges) {
         }
         return p;
     }
-    //return false if cant complete
+
+    //function to union the 2 nodes
     function union(n1, n2) {
         let p1 = find(n1);
         let p2 = find(n2);
+        //if both parents are equal,
         if (p1 === p2) {
             return false;
         }
+        //update ranking
         if (rank[p1] > rank[p2]) {
             parent[p2] = p1;
             rank[p1] += rank[p2];
@@ -28,11 +37,11 @@ var findRedundantConnection = function(edges) {
         }
         return true;
     }
-
     for (let [n1, n2] of edges) {
         if (!union(n1, n2)) {
             return [n1, n2];
         }
     }
+
     return [];
 };

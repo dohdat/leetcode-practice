02e80282834f532c807c60f1731a15177f01b833
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-18 12:10:18 -0700

    Time: 369 ms (70.31%), Space: 70.9 MB (73.44%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.js
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.js
@@ -1,12 +1,27 @@
 var maxProbability = function(n, edges, succProb, start, end) {
-    var ret = new Array(n).fill(0);
-    ret[start] = 1;
-    for (var i = 0; i <= n; i++) {
-        for (var j = 0; j < edges.length; ++j) {
-            var e = edges[j];
-            if (ret[e[1]] < ret[e[0]] * succProb[j]) ret[e[1]] = ret[e[0]] * succProb[j];
-            else if (ret[e[0]] < ret[e[1]] * succProb[j]) ret[e[0]] = ret[e[1]] * succProb[j];
+    let preMap = new Map();
+    let dists = new Array(n).fill(-Infinity);
+    for (let i = 0; i < edges.length; i++) {
+        let [from, to] = edges[i];
+        let cost = succProb[i];
+        preMap.set(from, (preMap.get(from) || []).concat([[to, cost]]));
+        preMap.set(to, (preMap.get(to) || []).concat([[from, cost]]));
+    }
+
+    let maxHeap = new MaxPriorityQueue({ priority: (x) => x[1] });
+    maxHeap.enqueue([start, 1]);
+    while (!maxHeap.isEmpty()) {
+        let [node, p] = maxHeap.dequeue().element;
+        if (node === end) return p;
+        if (dists[node] > p) continue;
+        let visiting = preMap.get(node) || [];
+
+        for (let [nei, cost] of visiting) {
+            if (p * cost > dists[nei]) {
+                dists[nei] = p * cost;
+                maxHeap.enqueue([nei, dists[nei]]);
+            }
         }
     }
-    return ret[end];
+    return 0;
 };

0a87916f8c3ec3a4add7a6df314c1594b2a7d9b6
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-19 10:49:37 -0700

    Time: 85 ms (50.38%), Space: 41.8 MB (69.07%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let memo = [];
+    function dfs(step, end) {
+        if (step === end) {
+            return 1;
+        }
+        if (end < step) {
+            return 0;
+        }
+        if (memo[step]) {
+            return memo[step];
+        }
+        memo[step] = dfs(step + 1, end) + dfs(step + 2, end);
+        return memo[step];
+    }
+    return dfs(0, n);
+};

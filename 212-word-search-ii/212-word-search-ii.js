7e1f2b0103c5e715207bdde8a5f27f59d146fe9b
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-19 10:30:08 -0700

    Time: 2295 ms (34.79%), Space: 59.3 MB (10.67%) - LeetHub

---
+++ 212-word-search-ii/212-word-search-ii.js
@@ -0,0 +1,60 @@
+/**
+ * @param {character[][]} board
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = function(board, words) {
+    let rows = board.length;
+    let cols = board[0].length;
+    let res = new Set();
+    let visited = new Array(rows).fill(false).map(() => new Array(cols).fill(false));
+
+    //Trie Implementation
+    function TrieNode() {
+        this.children = new Map();
+        this.end = false;
+    }
+
+    let root = new TrieNode();
+
+    function add(word) {
+        let cur = root;
+        for (let c of word) {
+            if (!cur.children.has(c)) {
+                cur.children.set(c, new TrieNode());
+            }
+            cur = cur.children.get(c);
+        }
+        cur.end = true;
+    }
+
+    //add all the words to Trie
+    for (let word of words) {
+        add(word);
+    }
+
+    function dfs(r, c, node, word) {
+        if (r < 0 || r >= rows || c < 0 || c >= cols || visited[r][c] || !node.children.get(board[r][c])) {
+            return;
+        }
+
+        word += board[r][c];
+        visited[r][c] = true;
+        node = node.children.get(board[r][c]);
+        if (node.end) res.add(word);
+
+        dfs(r + 1, c, node, word);
+        dfs(r - 1, c, node, word);
+        dfs(r, c + 1, node, word);
+        dfs(r, c - 1, node, word);
+        //backtrack
+        visited[r][c] = false;
+    }
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            dfs(r, c, root, '');
+        }
+    }
+    return [...res];
+};

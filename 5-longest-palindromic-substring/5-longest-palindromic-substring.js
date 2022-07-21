9b4ea1349a72fcea0b6cfc6482ed22f89f01c6cd
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-21 09:46:59 -0700

    Time: 127 ms (84.55%), Space: 46.2 MB (54.51%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -5,28 +5,19 @@
 var longestPalindrome = function(s) {
     let res = '';
     let resLen = 0;
+    function expand(l, r) {
+        while (l >= 0 && r < s.length && s[l] === s[r]) {
+            if (r - l + 1 > resLen) {
+                res = s.substring(l, r + 1);
+                resLen = r - l + 1;
+            }
+            l--;
+            r++;
+        }
+    }
     for (let i = 0; i < s.length; i++) {
-        //check odd length
-        let [l, r] = [i, i];
-        while (l >= 0 && r < s.length && s[l] === s[r]) {
-            if (r - l + 1 > resLen) {
-                res = s.substring(l, r + 1);
-                resLen = r - l + 1;
-            }
-            l--;
-            r++;
-        }
-        //check even length
-        l = i;
-        r = i + 1;
-        while (l >= 0 && r < s.length && s[l] === s[r]) {
-            if (r - l + 1 > resLen) {
-                res = s.substring(l, r + 1);
-                resLen = r - l + 1;
-            }
-            l--;
-            r++;
-        }
+        expand(i, i);
+        expand(i, i + 1);
     }
     return res;
 };

50d39cde8962f03514f45cf4e4a94f33c1fe66c4
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-07-27 09:59:44 -0700

    Time: 116 ms (59.00%), Space: 45.3 MB (28.40%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -3,15 +3,17 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-    let arr = [];
     let rows = matrix.length;
     let cols = matrix[0].length;
+    let res = [];
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < cols; c++) {
-            if (matrix[r][c] === 0) arr.push([r, c]);
+            if (matrix[r][c] === 0) {
+                res.push([r, c]);
+            }
         }
     }
-    for (let [r, c] of arr) {
+    for (let [r, c] of res) {
         for (let i = 0; i < rows; i++) {
             matrix[i][c] = 0;
         }

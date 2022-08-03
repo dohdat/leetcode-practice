a424b7e8224df02eaad2a5af7097d93d1ea54899
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-08-03 11:35:54 -0700

    Time: 314 ms (76.67%), Space: 63.7 MB (67.85%) - LeetHub

---
+++ 739-daily-temperatures/739-daily-temperatures.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    let res = new Array(temperatures.length).fill(0);
+    let stack = [];
+    for (let i = 0; i < temperatures.length; i++) {
+        let top = stack[stack.length - 1];
+        while (stack.length && temperatures[top] < temperatures[i]) {
+            let idx = stack.pop();
+            res[idx] = i - idx;
+            top = stack[stack.length - 1];
+        }
+        stack.push(i);
+    }
+    return res;
+};

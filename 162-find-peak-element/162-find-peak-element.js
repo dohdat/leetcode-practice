e042c8f3c8d660c35afc7077335d9bb03bfdd47b
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-08-08 11:56:37 -0700

    Time: 74 ms (76.13%), Space: 42.1 MB (69.79%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.js
+++ 162-find-peak-element/162-find-peak-element.js
@@ -3,12 +3,14 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-  for (let i = 0; i < nums.length; i++) {
-    let c = nums[i];
-    let next = nums[i + 1];
-    if (c > next) {
-      return i;
+  let [left, right] = [0, nums.length - 1];
+  while (left < right) {
+    let mid = Math.floor((right + left) / 2);
+    if (nums[mid] > nums[mid + 1]) {
+      right = mid;
+    } else {
+      left = mid + 1;
     }
   }
-  return nums.length - 1;
+  return left;
 };

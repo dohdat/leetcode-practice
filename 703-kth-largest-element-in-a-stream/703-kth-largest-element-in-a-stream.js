82f55d56859839e4306c41a67951cdb21623390f
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-08-16 15:47:19 -0700

    Time: 228 ms (57.71%), Space: 52.1 MB (41.19%) - LeetHub

---
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function(k, nums) {
+    this.main = new MinPriorityQueue();
+    for (let i = 0; i < nums.length; i++) {
+        this.main.enqueue(nums[i]);
+    }
+    this.k = k;
+    while (this.main.size() > k) {
+        this.main.dequeue().element;
+    }
+};
+
+/**
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+    this.main.enqueue(val);
+    if (this.main.size() > this.k) {
+        this.main.dequeue().element;
+    }
+    return this.main.front().element;
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */

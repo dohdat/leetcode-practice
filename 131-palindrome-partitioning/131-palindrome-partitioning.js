ae3f49e53b9f51d223206810c3b00343f7c6e3fa
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-08-19 12:04:39 -0700

    Time: 385 ms (63.96%), Space: 81.7 MB (31.76%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.js
+++ 131-palindrome-partitioning/131-palindrome-partitioning.js
@@ -5,11 +5,14 @@
 var partition = function(s) {
     let res = [];
     function checkPalindrome(str) {
-        let reversed = str
-            .split('')
-            .reverse()
-            .join('');
-        return reversed === str;
+        let left = 0;
+        let right = str.length - 1;
+        while (left < right) {
+            if (str[left] !== str[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
     }
     function backtrack(idx, path) {
         if (idx >= s.length) {

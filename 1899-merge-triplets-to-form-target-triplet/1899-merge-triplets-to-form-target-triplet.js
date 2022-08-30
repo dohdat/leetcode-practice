0a42984c9482cc7dab01ab1881e2ceea9f2604a0
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-08-30 10:49:00 -0700

    Time: 296 ms (15.74%), Space: 68.3 MB (86.80%) - LeetHub

---
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} triplets
+ * @param {number[]} target
+ * @return {boolean}
+ */
+var mergeTriplets = function(triplets, target) {
+  let res = new Set();
+  for (let t of triplets) {
+    if (t[0] > target[0] || t[1] > target[1] || t[2] > target[2]) {
+      continue;
+    }
+    for (let i = 0; i < t.length; i++) {
+      let v = t[i];
+      if (v === target[i]) {
+        res.add(i);
+      }
+    }
+  }
+  return res.size === 3;
+};

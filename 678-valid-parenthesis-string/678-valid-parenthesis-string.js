e264ac3ff28b07f6603906d8054323cd13ab0825
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-09-01 10:53:50 -0700

    Time: 94 ms (41.61%), Space: 42.1 MB (45.65%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.js
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.js
@@ -1,25 +1,14 @@
 function checkValidString(s) {
   let balance = 0;
-
-  // left to right : * becomes (
   for (let i = 0; i < s.length; i++) {
     s[i] === "(" || s[i] === "*" ? balance++ : balance--;
-
     if (balance < 0) return false;
   }
-
   if (balance === 0) return true;
-  // reset balance
   balance = 0;
-
-  // right to left : * becomes )
   for (let i = s.length - 1; i >= 0; i--) {
-    // if ) or *, increment balance, else decrement
     s[i] === ")" || s[i] === "*" ? balance++ : balance--;
-    // if ever balanced, not even wildcards can save this
     if (balance < 0) return false;
   }
-
-  // never found any problems, cool
   return true;
 }

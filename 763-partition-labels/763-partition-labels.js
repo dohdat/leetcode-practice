89874051089a06704b812f2e7111ed9dac7f88db
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-09-01 09:55:44 -0700

    Time: 113 ms (37.49%), Space: 43.2 MB (85.58%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number[]}
+ */
+var partitionLabels = function(s) {
+  let map = new Map();
+  let res = [];
+  let start = 0;
+  let end = 0;
+  for (let i = 0; i < s.length; i++) {
+    let cur = s[i];
+    if (map.get(cur)) {
+      map.set(cur, Math.max(map.get(cur), i));
+    } else {
+      map.set(cur, i);
+    }
+  }
+  for (let i = 0; i < s.length; i++) {
+    let cur = s[i];
+    end = Math.max(map.get(cur), end);
+    if (i + 1 > end) {
+      res.push([1 + end - start]);
+      start = i + 1;
+    }
+  }
+  return res;
+};

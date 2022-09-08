47047a9d49d4b0919fcaf0d69d5b05d86c64ed84
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-09-08 10:10:46 -0700

    Time: 89 ms (52.31%), Space: 42.2 MB (60.98%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.js
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.js
@@ -2,16 +2,16 @@ var tictactoe = function(moves) {
   // record and move on approach
   let rows = [0, 0, 0], cols = [0, 0, 0], dia = 0, antiDia = 0, count = 0;
   for (let [r, c] of moves) {
-    let value = count % 2 === 0 ? 1 : -1;
-    rows[r] += value;
-    cols[c] += value;
+    let val = count % 2 === 0 ? 1 : -1;
+    rows[r] += val;
+    cols[c] += val;
 
     if (r + c === 2) {
-      antiDia += value;
+      antiDia += val;
     }
 
     if (r === c) {
-      dia += value;
+      dia += val;
     }
     if ([rows[r], cols[c], antiDia, dia].includes(3)) {
       return "A";

526337b16b9ae784781c5ec06ef337ba19ab494e
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-09-08 10:10:19 -0700

    Time: 100 ms (34.10%), Space: 42.6 MB (34.39%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.js
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.js
@@ -1,84 +1,27 @@
-/**
- * @param {number[][]} moves
- * @return {string}
- */
 var tictactoe = function(moves) {
-  let size = 3;
-  let board = new Array(size).fill("").map(_ => new Array(size).fill(""));
-  let player = "A";
-  function checkDia(player) {
-    let diaCount = 0;
-    let antiDiaCout = 0;
-    let dia = [[0, 0], [1, 1], [2, 2]];
-    let antiDia = [[0, 2], [1, 1], [2, 0]];
-    for (let [r, c] of dia) {
-      if (board[r][c] === player) {
-        diaCount++;
-      }
-      if (diaCount === size) {
-        return true;
-      }
-    }
-    for (let [r, c] of antiDia) {
-      if (board[r][c] === player) {
-        antiDiaCout++;
-      }
-      if (antiDiaCout === size) {
-        return true;
-      }
-    }
-    return false;
-  }
-  function checkRows(player) {
-    for (let r of board) {
-      let count = r.filter(x => x === player).length;
-      if (count === size) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  function checkCols(player) {
-    let directions = [
-      [[0, 0], [1, 0], [2, 0]],
-      [[0, 1], [1, 1], [2, 1]],
-      [[0, 2], [1, 2], [2, 2]]
-    ];
-    for (let col of directions) {
-      let count = 0;
-      for (let [r, c] of col) {
-        if (board[r][c] === player) {
-          count++;
-        }
-        if (count === size) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
-  function checkDraw() {
-    let count = 0;
-    for (let r = 0; r < board.length; r++) {
-      for (let c = 0; c < board[0].length; c++) {
-        if (board[r][c]) {
-          count++;
-        }
-      }
-    }
-    return count === 9;
-  }
-
+  // record and move on approach
+  let rows = [0, 0, 0], cols = [0, 0, 0], dia = 0, antiDia = 0, count = 0;
   for (let [r, c] of moves) {
-    let curPlayer = player === "A" ? "X" : "O";
-    board[r][c] = curPlayer;
-    if (checkRows(curPlayer) || checkCols(curPlayer) || checkDia(curPlayer)) {
-      return player;
-    }
-    player = player === "A" ? "B" : "A";
-  }
+    let value = count % 2 === 0 ? 1 : -1;
+    rows[r] += value;
+    cols[c] += value;
 
-  return checkDraw() ? "Draw" : "Pending";
+    if (r + c === 2) {
+      antiDia += value;
+    }
+
+    if (r === c) {
+      dia += value;
+    }
+    if ([rows[r], cols[c], antiDia, dia].includes(3)) {
+      return "A";
+    } else if ([rows[r], cols[c], antiDia, dia].includes(-3)) {
+      return "B";
+    }
+    count++;
+  }
+  if (count < 9) {
+    return "Pending";
+  }
+  return "Draw";
 };

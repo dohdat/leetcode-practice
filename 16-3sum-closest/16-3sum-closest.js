e6c3b761d0ff2067621eb2c366d799e392b0f05b
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-09-21 09:19:18 -0700

    Time: 158 ms (93.90%), Space: 43.9 MB (95.34%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.js
+++ 16-3sum-closest/16-3sum-closest.js
@@ -6,11 +6,14 @@
 var threeSumClosest = function(nums, target) {
   nums.sort((a, b) => a - b);
   let min = Infinity;
-  for (let i = 0; i < nums.length - 2; i++) {
+  for (let i = 0; i < nums.length - 1; i++) {
     let left = i + 1, right = nums.length - 1;
     while (left < right) {
       let cur = nums[i] + nums[left] + nums[right];
       if (Math.abs(cur - target) < Math.abs(min - target)) min = cur;
+      if (cur === target) {
+        return target;
+      }
       if (cur > target) {
         right--;
       } else {

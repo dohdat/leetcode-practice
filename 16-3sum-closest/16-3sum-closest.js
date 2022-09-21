103e59b372080276228c054f289cd1a6bc4a958e
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-09-21 09:16:46 -0700

    Time: 221 ms (59.32%), Space: 44 MB (88.26%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+  nums.sort((a, b) => a - b);
+  let min = Infinity;
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1, right = nums.length - 1;
+    while (left < right) {
+      let cur = nums[i] + nums[left] + nums[right];
+      if (Math.abs(cur - target) < Math.abs(min - target)) min = cur;
+      if (cur > target) {
+        right--;
+      } else {
+        left++;
+      }
+    }
+  }
+  return min;
+};

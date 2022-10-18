f86a6aecdac550e9089593d10940fd92b1102976
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-10-18 14:53:28 -0700

    Time: 184 ms (15.03%), Space: 45.5 MB (38.08%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+  let min = 1;
+  let max = Math.max(...piles);
+  let best = max;
+
+  function time(speed) {
+    return piles.reduce((sum, pile) => sum + Math.ceil(pile / speed), 0);
+  }
+  while (min <= max) {
+    const mid = Math.floor((min + max) / 2);
+    if (time(mid) <= h) {
+      best = mid;
+      max = mid - 1;
+    } else {
+      min = mid + 1;
+    }
+  }
+  return best;
+};

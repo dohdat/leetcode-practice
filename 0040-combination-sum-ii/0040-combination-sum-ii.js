577cf28c63e6f1a1b4438a6d297ecb68021f6f8f
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-10-20 11:02:11 -0700

    Time: 129 ms (31.83%), Space: 44.4 MB (53.39%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function(nums, target) {
+  if (!nums || nums.length === 0) return [];
+  nums.sort((a, b) => a - b);
+  let res = [];
+  function backtrack(idx, path, total) {
+    if (idx > nums.length || total > target) {
+      return;
+    }
+    if (total === target) {
+      res.push([...path]);
+    }
+    for (let i = idx; i < nums.length; i++) {
+      if (i > idx && nums[i] === nums[i - 1]) {
+        continue;
+      }
+      path.push(nums[i]);
+      backtrack(i + 1, path, total + nums[i]);
+      path.pop();
+    }
+  }
+  backtrack(0, [], 0);
+  return res;
+};

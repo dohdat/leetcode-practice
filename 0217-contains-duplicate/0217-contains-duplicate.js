15050c856abbab9f5b79d7fb9acfe15ee0c64229
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-11-18 11:41:08 -0800

    Time: 111 ms (84.03%), Space: 50.9 MB (53.96%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,14 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-  let preMap = new Map();
-  for (let num of nums) {
-    preMap.set(num, (preMap.get(num) || 0) + 1);
-  }
-  for (let [num, freq] of preMap) {
-    if (freq > 1) {
-      return true;
-    }
-  }
-  return false;
+  let set = new Set(nums);
+  return set.size !== nums.length;
 };

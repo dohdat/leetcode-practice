5b64c501a5db13c33c7b1a1f1c2e99facad0b976
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-11-21 15:45:14 -0800

    Time: 66 ms (96.21%), Space: 42.6 MB (56.82%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -3,15 +3,16 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-  let prevMax = nums[0];
+  let preMax = nums[0];
+  let preMin = nums[0];
   let res = nums[0];
-  let prevMin = nums[0];
+
   for (let i = 1; i < nums.length; i++) {
     let n = nums[i];
-    let curMax = Math.max(n * prevMax, n * prevMin, n);
-    let curMin = Math.min(n * prevMax, n * prevMin, n);
-    prevMax = curMax;
-    prevMin = curMin;
+    let curMax = Math.max(n, n * preMax, n * preMin);
+    let curMin = Math.min(n, n * preMax, n * preMin);
+    preMax = curMax;
+    preMin = curMin;
     res = Math.max(curMax, res);
   }
   return res;

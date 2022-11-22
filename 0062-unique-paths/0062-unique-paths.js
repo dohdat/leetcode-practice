25f923036f62134c2f6a841b8c9226f5ff33d672
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-11-21 17:41:02 -0800

    Time: 105 ms (33.25%), Space: 44.5 MB (7.46%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.js
@@ -0,0 +1,9 @@
+const uniquePaths = function(m, n, memo = new Map()) {
+  const key = m + "," + n;
+  if (memo.has(key)) return memo.get(key);
+  if (m === 1 || n === 1) return 1;
+  if (m === 0 || n === 0) return 0;
+
+  memo.set(key, uniquePaths(m - 1, n, memo) + uniquePaths(m, n - 1, memo));
+  return memo.get(key);
+};

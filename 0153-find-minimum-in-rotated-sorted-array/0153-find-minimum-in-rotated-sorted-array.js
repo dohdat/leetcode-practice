5c46f776a33606a91862d7bc680294bf35c236e7
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-11-22 13:08:59 -0800

    Time: 86 ms (68.62%), Space: 42.2 MB (34.36%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -5,7 +5,7 @@
 var findMin = function(nums) {
   let left = 0;
   let right = nums.length - 1;
-  let res = Infinity;
+  let res = nums[0];
   while (left <= right) {
     if (nums[left] <= nums[right]) {
       res = Math.min(res, nums[left]);
@@ -21,3 +21,16 @@ var findMin = function(nums) {
   }
   return res;
 };
+
+/**
+Q) Why do we check nums[left] <= nums[right] before checking between nums[left] and res?
+A) This is a case, where everything is already sorted, no rotation, then we can just compare between res and left most values in nums
+Q) Why do we check res = Math.min(res, nums[mid])?
+A) Because we need to update res with min between res or mid value
+Q) What does this mean if nums[mid] >= nums[left] then left = mid+1?
+A) To determine if we are going to search to the left or right. If it's sorted on the left portion, search right portion
+Q) Time complexity?
+A) O(log n): binary search
+Q) Space Complexity?
+A) O(1) not using not extra spaces
+*/

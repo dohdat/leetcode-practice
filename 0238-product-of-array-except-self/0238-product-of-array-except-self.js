fd9034123ba9678eeb1f56eadb9324f587a2ccfd
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-11-22 10:05:25 -0800

    Time: 176 ms (65.18%), Space: 53.3 MB (83.20%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,13 +2,11 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-//5-1-1
 var productExceptSelf = function(nums) {
   let n = nums.length - 1;
   let res = new Array(n).fill(1);
   let preFix = 1;
   let postFix = 1;
-  //calculate prefix
   for (let i = 0; i <= n; i++) {
     res[i] = preFix;
     preFix *= nums[i];
@@ -17,6 +15,5 @@ var productExceptSelf = function(nums) {
     res[i] *= postFix;
     postFix *= nums[i];
   }
-  //calcualte postfix
   return res;
 };

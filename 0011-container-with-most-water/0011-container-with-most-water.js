f5153dd7614d687b1bb6c59325db5dafbbbaa47c
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-11-23 14:59:21 -0800

    Time: 81 ms (93.26%), Space: 49.3 MB (83.82%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+  let left = 0;
+  let right = height.length - 1;
+  let res = 0;
+  while (left <= right) {
+    let curArea = Math.min(height[right], height[left]) * (right - left);
+    if (height[left] > height[right]) {
+      right--;
+    } else {
+      left++;
+    }
+    res = Math.max(res, curArea);
+  }
+  return res;
+};

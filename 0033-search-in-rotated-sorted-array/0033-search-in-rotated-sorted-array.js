fb44be94e04144ca26677c4d9bfd9ddf917ad3ee
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-11-23 17:05:06 -0800

    Time: 81 ms (77.44%), Space: 42 MB (75.83%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -11,20 +11,15 @@ var search = function(nums, target) {
     if (nums[mid] === target) {
       return mid;
     }
-    //check if left side is sorted
-    if (nums[left] <= nums[mid]) {
-      //if smallest <= target <= biggest
+
+    if (nums[mid] >= nums[left]) {
       if (nums[left] <= target && target <= nums[mid]) {
-        //target is in the left side
         right = mid - 1;
       } else {
         left = mid + 1;
       }
     } else {
-      //otherwise, right side is sorted
-      //if smallest <= target <= biggest
       if (nums[mid] <= target && target <= nums[right]) {
-        //target is in the right
         left = mid + 1;
       } else {
         right = mid - 1;

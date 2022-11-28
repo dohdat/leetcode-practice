bbed51d0247bc19993736537c22329e7c731242c
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-11-28 14:21:24 -0800

    Time: 94 ms (71.39%), Space: 42.6 MB (50.19%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.js
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -3,5 +3,11 @@
  * @return {number}
  */
 var hammingWeight = function(n) {
-  return n.toString(2).split("").reduce((a, b) => Number(a) + Number(b), 0);
+  let count = 0;
+  while (n !== 0) {
+    let bitComp = n & 1; //1 &1 will return 1, 0 &1 will return 0
+    if (bitComp === 1) count++;
+    n >>>= 1;
+  }
+  return count;
 };

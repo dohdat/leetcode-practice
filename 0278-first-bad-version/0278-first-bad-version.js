6ec996719d3a3a67194859cab366d683b8149e34
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-12-01 11:34:34 -0800

    Time: 905 ms (7.13%), Space: 41.4 MB (95.43%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+  /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+  return function(n) {
+    let left = 1;
+    let right = n;
+    while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+      if (isBadVersion(mid)) {
+        let found = mid;
+        while (isBadVersion(found)) {
+          found -= 1;
+        }
+        return found + 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return -1;
+  };
+};

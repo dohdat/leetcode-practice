f47fa489c658f265404eb7f52a8faf3e836d6362
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-12-01 11:36:29 -0800

    Time: 84 ms (68.57%), Space: 41.5 MB (85.00%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.js
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -20,18 +20,14 @@ var solution = function(isBadVersion) {
   return function(n) {
     let left = 1;
     let right = n;
-    while (left <= right) {
+    while (left < right) {
       let mid = Math.floor((left + right) / 2);
       if (isBadVersion(mid)) {
-        let found = mid;
-        while (isBadVersion(found)) {
-          found -= 1;
-        }
-        return found + 1;
+        right = mid;
       } else {
         left = mid + 1;
       }
     }
-    return -1;
+    return left;
   };
 };

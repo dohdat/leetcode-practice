2945803948506e773dabf5869d25a096ad8927d2
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-12-01 12:07:13 -0800

    Time: 303 ms (5.85%), Space: 51.4 MB (5.22%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+  let q = new MinPriorityQueue({ priority: x => x });
+  let res = [];
+  for (let n of nums) {
+    q.enqueue(Math.pow(n, 2));
+  }
+  for (let i = 0; i < nums.length; i++) {
+    let c = q.dequeue().element;
+    res.push(c);
+  }
+  return res;
+};

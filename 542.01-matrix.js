cdc29e7dbb6b7a67be973e76f37d4f5bca8dbb8d
Dat Do <ddo@splunk.com>
2022-12-08 14:40:32 -0800

    main: Added

--- 542.01-matrix.js
+++ 542.01-matrix.js
@@ -12,6 +12,10 @@
 var updateMatrix = function(mat) {
 	let rows = mat.length;
 	let cols = mat[0].length;
+
+	for (let r = 0; r < rows; r++) {
+		for (let c = 0; c < cols; c++) {}
+	}
 };
 // @lc code=end
 

2c7e04b22da7f04093fad000e587026b7ac5e10b
Dat Do <30649150+dohdat@users.noreply.github.com>
2022-12-08 16:28:24 -0800

    Time: 83 ms (99.85%), Space: 49.9 MB (96.89%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @param {number[][]} trust
+ * @return {number}
+ */
+var findJudge = function(n, trust) {
+  let trustCounts = new Array(n + 1).fill(0);
+  for (let [a, b] of trust) {
+    trustCounts[a]--;
+    trustCounts[b]++;
+  }
+  for (let i = 1; i < trustCounts.length; i++) {
+    if (trustCounts[i] === n - 1) {
+      return i;
+    }
+  }
+  return -1;
+};

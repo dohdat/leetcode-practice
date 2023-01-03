0ccf073a0ed1d08f83faffec8e3fea1c5fb455b1
Dat Do <30649150+dohdat@users.noreply.github.com>
2023-01-03 14:51:50 -0800

    Time: 121 ms (52.11%), Space: 44.7 MB (80.44%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -1,51 +1,49 @@
-/**
- * @param {character[][]} board
- * @return {boolean}
- */
-var isValidSudoku = function(board) {
+function isValidSudoku(board) {
   let rows = board.length;
   let cols = board[0].length;
-  function hasUniqueDigits(array) {
-    let set = new Set();
-    for (let v of array) {
+  let set = new Set();
+
+  // Each row must contain the digits 1-9
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      let v = board[r][c];
       if (v !== ".") {
         if (set.has(v)) return false;
         set.add(v);
       }
     }
-    return true;
+    set.clear();
   }
-  //each row must contain digits 1-9
-  for (let r = 0; r < rows; r++) {
-    if (!hasUniqueDigits(board[r])) {
-      return false;
-    }
-  }
-  //each column must contain digits 1-9
+
+  // Each column must contain the digits 1-9
   for (let c = 0; c < cols; c++) {
-    let column = [];
     for (let r = 0; r < rows; r++) {
-      column.push(board[r][c]);
+      let v = board[r][c];
+      if (v !== ".") {
+        if (set.has(v)) return false;
+        set.add(v);
+      }
     }
-    if (!hasUniqueDigits(column)) {
-      return false;
-    }
+    set.clear();
   }
 
   // Each region (3x3 grid) must contain the digits 1-9
   for (let r = 0; r < 3; r++) {
     for (let c = 0; c < 3; c++) {
-      let region = [];
       for (let k = 0; k < 3; k++) {
         for (let l = 0; l < 3; l++) {
           let rowIndex = r * 3 + k;
           let colIndex = c * 3 + l;
-          region.push(board[rowIndex][colIndex]);
+          let v = board[rowIndex][colIndex];
+          if (v !== ".") {
+            if (set.has(v)) return false;
+            set.add(v);
+          }
         }
       }
-      if (!hasUniqueDigits(region)) return false;
+      set.clear();
     }
   }
 
   return true;
-};
+}

b2152891d0a5c7883ed3e1eff396c199c5d2d856
Dat Do <30649150+dohdat@users.noreply.github.com>
2023-01-04 10:09:05 -0800

    Time: 615 ms (43.60%), Space: 65 MB (73.25%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -1,15 +1,24 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
 var dailyTemperatures = function(temperatures) {
+  // stack to store indices of temperatures
   let stack = [];
   let n = temperatures.length;
+  // result array to store distance between each temperature and the next larger temperature
   let res = new Array(n).fill(0);
   for (let i = 0; i < n; i++) {
+    // current temperature
     let curTemp = temperatures[i];
+    // while there is a next larger temperature
     while (
       stack.length > 0 && curTemp > temperatures[stack[stack.length - 1]]
     ) {
+      // pop top temperature from stack
       let index = stack.pop();
+      // update result array with distance between current temperature and next larger temperature
       res[index] = i - index;
     }
+    // push current temperature and its index onto stack
     stack.push(i);
   }
   return res;

e044c24d5c331fc939ca9194a713a11d0a8aa902
Dat Do <30649150+dohdat@users.noreply.github.com>
2023-01-09 17:18:51 -0800

    Time: 69 ms (81.85%), Space: 43 MB (65.98%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -1,18 +1,30 @@
-/**
- * @param {number[]} numbers
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = function(nums, target) {
-  let res = [];
-  let n = nums.length;
-  let newSet = new Set();
-  for (let i = 0; i < n; i++) {
-    let missing = target - nums[i];
-    if (!newSet.has(missing) && nums.includes(missing)) {
-      return [i + 1, nums.indexOf(missing, i + 1) + 1];
-    } else {
-      newSet.add(missing);
+function twoSum(numbers, target) {
+  function binarySearch(missing) {
+    let left = 0;
+    let right = numbers.length - 1;
+    while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+      if (missing === numbers[mid]) {
+        return mid;
+      }
+      if (missing < numbers[mid]) {
+        right = mid - 1;
+      } else if (missing > numbers[mid]) {
+        left = mid + 1;
+      }
+    }
+    return -1;
+  }
+  for (let i = 0; i < numbers.length; i++) {
+    let cur = numbers[i];
+    let missing = target - cur;
+    let index = binarySearch(missing);
+    if (index !== -1) {
+      if (index === i) {
+        continue;
+      }
+      return [i + 1, index + 1].sort((a, b) => a - b);
     }
   }
-};
+  return [-1, -1];
+}

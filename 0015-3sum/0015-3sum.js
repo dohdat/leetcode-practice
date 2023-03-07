52f7c875cc21b00596130237dfc602247c0d1005
Dat Do <30649150+dohdat@users.noreply.github.com>
2023-03-07 09:41:58 -0800

    Time: 158 ms (93.94%), Space: 56.4 MB (52.05%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -1,25 +1,22 @@
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
 var threeSum = function(nums) {
-  nums.sort((a, b) => a - b);
-  let numSet = new Set();
   let res = [];
-  for (let i = 0; i < nums.length; i++) {
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > 0) break;
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
     let left = i + 1;
-    let right = nums.length;
+    let right = nums.length - 1;
     while (left < right) {
-      let target = nums[i] + nums[left] + nums[right];
-      if (target === 0) {
-        let resStr = [nums[i], nums[left], nums[right]].toString();
-        if (!numSet.has(resStr)) {
-          res.push([nums[i], nums[left], nums[right]]);
-          numSet.add(resStr);
-        }
+      let sum = nums[i] + nums[left] + nums[right];
+      if (sum === 0) {
+        res.push([nums[i], nums[left], nums[right]]);
+        while (left < right && nums[left] === nums[left + 1])
+          left++;
+        while (left < right && nums[right] === nums[right - 1])
+          right--;
         left++;
         right--;
-      } else if (target < 0) {
+      } else if (sum < 0) {
         left++;
       } else {
         right--;

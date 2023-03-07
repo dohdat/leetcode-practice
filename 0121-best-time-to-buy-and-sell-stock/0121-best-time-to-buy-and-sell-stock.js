cb509197ab40e0064844546b295819e78db69a33
Dat Do <30649150+dohdat@users.noreply.github.com>
2023-03-07 09:53:18 -0800

    Time: 77 ms (92.10%), Space: 51.2 MB (95.48%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,13 +3,14 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
+  let localMin = prices[0];
   let profit = 0;
-  let minVal = Infinity;
-  for (let p of prices) {
-    if (p < minVal) {
-      minVal = p;
-    } else if (p - minVal > profit) {
-      profit = p - minVal;
+  let n = prices.length;
+  for (let i = 0; i < n; i++) {
+    if (prices[i] < localMin) {
+      localMin = prices[i];
+    } else if (prices[i] - localMin > profit) {
+      profit = prices[i] - localMin;
     }
   }
   return profit;

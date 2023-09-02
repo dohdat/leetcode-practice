76867a14a9774656dd95c30cb3a85ad5682ed3e0
dohdat@gmail.com <dohdat@gmail.com>
2023-09-02 10:20:53 -0700

    Added getting the current schedule

--- TutorCom/TutorSchedule.py
+++ TutorCom/TutorSchedule.py
@@ -17,13 +17,13 @@ def send_request(url):
 # Calculate the date of the first Monday of next week
 today = datetime.now()
 days_until_monday = (7 - today.weekday()) % 7
-next_monday = today + timedelta(days=days_until_monday - 1)  # Add 7 days to ensure it's the next week
+next_monday = today + timedelta(days=days_until_monday - 1)
 
 # Format the date in the desired format (MM/DD/YYYY)
 formatted_date = next_monday.strftime("%m/%d/%Y")
 print(formatted_date)
-# Define the URL with the calculated date
 
+# Define the URL with the calculated date
 urls = []
 
 # Iterate through the table data and generate URLs
@@ -60,6 +60,19 @@ with ThreadPoolExecutor(max_workers=num_threads) as executor:
 end_time = time.time()
 total_time = end_time - start_time
 print(f"All requests completed. Total Time: {total_time:.2f} seconds")
+
+
+
+# Call the GET endpoint to get scheduled dates, selected date is formatted_date - 7 days
+selected_date = (today + timedelta(days=days_until_monday - 8)).strftime("%m/%d/%Y")
+get_url = f"https://prv.tutor.com/nGEN/Tools/ScheduleManager_v2/default.aspx?SelectedDate={selected_date}&DaysToAdd=7"
+
+# Send the GET request
+response = session.get(get_url)
+print(f"GET request to {get_url}. Status Code: {response.status_code}")
+
+# Print the response text
+print(response.text)
 
 
 

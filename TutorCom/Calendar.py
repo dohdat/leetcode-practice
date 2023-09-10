7e0a6260e520a1d8ba44c9e55e9136b5a864c6b5
dohdat@gmail.com <dohdat@gmail.com>
2023-09-09 23:14:30 -0700

    Fix Google calendar not getting scheduled.

--- TutorCom/Calendar.py
+++ TutorCom/Calendar.py
@@ -9,69 +9,87 @@ from google.auth.transport.requests import Request
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly', 'https://www.googleapis.com/auth/calendar.events', 'https://www.googleapis.com/auth/calendar']
+SCOPES = [
+    "https://www.googleapis.com/auth/calendar.readonly",
+    "https://www.googleapis.com/auth/calendar.events",
+    "https://www.googleapis.com/auth/calendar",
+]
+
+os.chdir(r"C:\Users\dohda\leetcode-practice\TutorCom")
+
 
 def get_google_calendar_service():
     """Authenticate and get the Google Calendar service."""
     creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
+        with open("token.json", "w") as token:
             token.write(creds.to_json())
-    return build('calendar', 'v3', credentials=creds)
+    return build("calendar", "v3", credentials=creds)
 
-def list_upcoming_events(calendar_id='primary', maxResults=25):
+
+def list_upcoming_events(calendar_id="primary", maxResults=25):
     """List the next 10 events on the user's calendar."""
     try:
         service = get_google_calendar_service()
-        now = datetime.datetime.utcnow().isoformat() + 'Z'
-        print(f'Getting the upcoming {maxResults} events')
-        events_result = service.events().list(calendarId=calendar_id, timeMin=now,
-                                               maxResults=maxResults, singleEvents=True,
-                                               orderBy='startTime').execute()
-        events = events_result.get('items', [])
+        now = datetime.datetime.utcnow().isoformat() + "Z"
+        print(f"Getting the upcoming {maxResults} events")
+        events_result = (
+            service.events()
+            .list(
+                calendarId=calendar_id,
+                timeMin=now,
+                maxResults=maxResults,
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
+        )
+        events = events_result.get("items", [])
         res = []
         if not events:
-            print('No upcoming events found.')
+            print("No upcoming events found.")
             return
 
         for event in events:
-            start = event.get('start', {}).get('dateTime', event.get('start', {}).get('date'))
-            print(start, event.get('summary'))
+            start = event.get("start", {}).get(
+                "dateTime", event.get("start", {}).get("date")
+            )
+            print(start, event.get("summary"))
             res.append(start)
         return res
 
     except HttpError as error:
-        print('An error occurred: %s' % error)
+        print("An error occurred: %s" % error)
+
 
 def create_event(calendar_id, start_time, end_time, event_summary):
     """Create a Calendar event."""
     try:
         service = get_google_calendar_service()
         event = {
-            'summary': event_summary,
-            'start': {
-                'dateTime': start_time,
-                'timeZone': 'America/New_York',
+            "summary": event_summary,
+            "start": {
+                "dateTime": start_time,
+                "timeZone": "America/New_York",
             },
-            'end': {
-                'dateTime': end_time,
-                'timeZone': 'America/New_York',
+            "end": {
+                "dateTime": end_time,
+                "timeZone": "America/New_York",
             },
             "transparency": "transparent",
         }
         event = service.events().insert(calendarId=calendar_id, body=event).execute()
         print(f'Event created: {event.get("htmlLink")}')
     except HttpError as error:
-        print('An error occurred: %s' % error)
+        print("An error occurred: %s" % error)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     list_upcoming_events()
-    
